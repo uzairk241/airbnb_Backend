0adfc70efa642e185ca97c6eb2b3a6b5c060e0bc
uzairk241 <m.uzairkhan241@gmail.com>
2023-10-10 18:58:00 +0500

    first commit

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ const app = express();
 
 
 
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(cookieParser());
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api",router)
 
