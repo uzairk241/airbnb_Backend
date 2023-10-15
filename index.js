d3d5d0397358d6530f77506b06f6ca704dbf521c
uzairk241 <m.uzairkhan241@gmail.com>
2023-10-15 22:41:14 +0500

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = require('./Routes/Router');
 require('dotenv').config();
 const app = express();
 
-app.use(cors({origin:"http://localhost:3000",credentials:true}));
+app.use(cors({origin:"https://airbnb-frontend-uzairk241.vercel.app",credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 
